a108e7b55b5e5a530a671ea0a413988b856064b9
aimtyaem <64774266+aimtyaem@users.noreply.github.com>
2020-09-21 11:30:14 +0200

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,10 @@
+import​ ​random​
 def main():
   print('You rolled a die')
 
 if __name__== "__main__":
   main()
+def roll()
+roll = 5
+roll​ ​=​ ​random​.​randint​(​1​,​6​)
+print​(​f'You rolled a ​{​roll​}​
