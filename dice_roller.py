a3c2383c5d30c31d5e45ce9baae2e09888ad17db
aimtyaem <64774266+aimtyaem@users.noreply.github.com>
2020-09-21 11:57:31 +0200

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -4,9 +4,16 @@ def main():
 
 if __name__== "__main__":
   main()
+if​ ​roll​ ​==​ ​1​:
+  ​print​(​f'You rolled a ​{​roll​}​! Critical Fail'​)
+elif​ ​roll​ ​==​ ​6​:
+  ​print​(​f'You rolled a ​{​roll})
+else:
+print​(​f'You rolled a ​{​roll})
 def roll()
 roll = 5
 dice_rolls​ ​=​ ​2​
+dice_sum​ ​=​ ​0​
 dice_sum​ ​+=​ ​roll
 ​for ​i​ ​in​ ​range​(​0​,​dice_rolls​):
 roll​ ​=​ ​random​.​randint​(​1​,​6​)
